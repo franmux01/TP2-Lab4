ea176b81b040c3336950a839c2fdb6b1bfa1e298
franmux01 <franciscomule2501@gmail.com>
2023-06-11 22:39:03 -0300

    Done

--- tp_laboratorio/Test/MovieTest.cs
+++ tp_laboratorio/Test/MovieTest.cs
@@ -6,22 +6,22 @@ public class MovieTests
     [Fact]
     public void TestMovieName()
     {
-        var movie = new Movie { MovieName = "Example Movie" };
-        Assert.Equal("Example Movie", movie.MovieName);
+        var movie = new Movie { MovieName = "Toy Story 3" };
+        Assert.Equal("Toy Story 3", movie.MovieName);
     }
 
     [Fact]
     public void TestMovieGenre()
     {
-        var movie = new Movie { MovieGenre = "Example Genre" };
-        Assert.Equal("Example Genre", movie.MovieGenre);
+        var movie = new Movie { MovieGenre = "Kids" };
+        Assert.Equal("Kids", movie.MovieGenre);
     }
 
     [Fact]
     public void TestActorName()
     {
-        var movie = new Actor { ActorName = "Example Actor" };
-        Assert.Equal("Example Actor", movie.ActorName);
+        var movie = new Actor { ActorName = "Woody" };
+        Assert.Equal("Woody", movie.ActorName);
     }
 }
 
