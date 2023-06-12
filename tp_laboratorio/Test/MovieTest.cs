034f37e68478e22e8ca535d104b018fbc71a8804
franmux01 <franciscomule2501@gmail.com>
2023-06-11 21:23:20 -0300

    2 test mas

--- tp_laboratorio/Test/MovieTest.cs
+++ tp_laboratorio/Test/MovieTest.cs
@@ -9,5 +9,19 @@ public class MovieTests
         var movie = new Movie { MovieName = "Example Movie" };
         Assert.Equal("Example Movie", movie.MovieName);
     }
+
+    [Fact]
+    public void TestMovieGenre()
+    {
+        var movie = new Movie { MovieGenre = "Example Genre" };
+        Assert.Equal("Example Genre", movie.MovieGenre);
+    }
+
+    [Fact]
+    public void TestActorName()
+    {
+        var movie = new Actor { ActorName = "Example Actor" };
+        Assert.Equal("Example Actor", movie.ActorName);
+    }
 }
 
